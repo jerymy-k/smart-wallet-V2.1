89a980e68fe1a82ca5883e3a6b5881d3a5d96a7d
jerymy-k <karimimoha0@gmail.com>
2025-12-23 16:55:22 +0100

    the connection with database Database class

---
+++ app/Core/Database.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static ?Database $instance = null;
+    private PDO $connection;
+
+    private string $host = "localhost";
+    private string $dbName = "smart_wallet";
+    private string $user = "root";
+    private string $pass = "kerymy";
+    private string $charset = "utf8mb4";
+
+    private function __construct()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset={$this->charset}";
+
+        try {
+            $this->connection = new PDO($dsn, $this->user, $this->pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance(): Database
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
