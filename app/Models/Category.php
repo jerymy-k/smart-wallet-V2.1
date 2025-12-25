096ef52af9b45995d417f3699cb00edfa8a1102b
jerymy-k <karimimoha0@gmail.com>
2025-12-25 09:34:37 +0100

    realese class category

---
+++ app/Models/Category.php
@@ -0,0 +1,52 @@
+<?php
+require_once __DIR__ . "/../Core/Database.php";
+use app\Core\Database;
+
+class Category
+{
+    private PDO $db;
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+    public function create(int $user_id, string $name, string $type = 'both'): bool
+    {
+        $sql = "INSERT INTO categories(user_id , name , type ) VALUES (?,?,?)";
+        $insert = $this->db->prepare($sql);
+        return $insert->execute([$user_id, $name, $type]);
+    }
+    public function getAllCategoriesByUser(int $user_id): ?array
+    {
+        $sql = "SELECT * FROM categories WHERE user_id = ? ORDER BY created_at DESC ";
+        $getall = $this->db->prepare($sql);
+        $getall->execute([$user_id]);
+        $AllCate = $getall->fetchAll();
+        return $AllCate;
+    }
+    public function updateCategorie(int $user_id, int $categorie_id, string $name, string $type = 'both'): bool
+    {
+        $sql = "UPDATE categories SET name = ?, type = ? WHERE id = ? AND user_id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$name, $type, $categorie_id, $user_id]);
+    }
+    public function deleteCategorie(int $categorie_id, string $user_id): bool
+    {
+        $sql = "DELETE FROM categories WHERE id = ? AND user_id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$categorie_id, $user_id]);
+    }
+    public function someCategories(?string $type, int $user_id): int
+    {
+        if ($type === NULL) {
+            $sql = "SELECT COUNT(id) AS someCat FROM categories WHERE user_id = ? ";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute([$user_id]);
+        } else {
+            $sql = "SELECT count(id) AS someCat FROM categories WHERE user_id = ? AND type = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute([$user_id, $type]);
+        }
+        $result = $stmt->fetch();
+        return $result['someCat'];
+    }
+}
